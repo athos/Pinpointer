7397ee4a0db79934628c121290eb56f76f1bd39a
OHTA Shogo <athos0220@gmail.com>
2016-07-17 15:43:37 +0900

    Add opts arg to public APIs

--- src/pinpointer/core.clj
+++ src/pinpointer/core.clj
@@ -19,27 +19,32 @@
   (apply str (concat (repeat start \space)
                      (repeat (- end start) \^))))
 
-(defn pinpoint-out [ed & {:keys [root]}]
-  (when-let [problems (::s/problems ed)]
-    (doseq [[i probs] (->> problems
-                           (group-by #(select-keys % [:val :in]))
-                           (map-indexed vector))
-            :let [[{:keys [val in]} probs] probs
-                  val (or root val)
-                  [start end] (extract val in)]]
-      (when (not= i 0)
-        (newline))
-      #_(print " Problem: ")
-      #_(prn (assoc problem :val val))
-      (println "   Input:" (pr-str val))
-      (printf  "        :%s\n" (wavy-line start end))
-      (print "Expected: ")
-      (doseq [[j {:keys [pred reason]}] (map-indexed vector probs)]
-        (when (not= j 0)
-          (print "          "))
-        (prn pred)
-        (when reason
-          (printf "(%s)\n" reason))))))
+(defn pinpoint-out
+  ([ed] (pinpoint-out ed {}))
+  ([ed {:keys [root]}]
+   (when-let [problems (::s/problems ed)]
+     (doseq [[i probs] (->> problems
+                            (group-by #(select-keys % [:val :in]))
+                            (map-indexed vector))
+             :let [[{:keys [val in]} probs] probs
+                   val (or root val)
+                   [start end] (extract val in)]]
+       (when (not= i 0)
+         (newline))
+       #_(print " Problem: ")
+       #_(prn (assoc problem :val val))
+       (println "   Input:" (pr-str val))
+       (printf  "        :%s\n" (wavy-line start end))
+       (print "Expected: ")
+       (doseq [[j {:keys [pred reason]}] (map-indexed vector probs)]
+         (when (not= j 0)
+           (print "          "))
+         (prn pred)
+         (when reason
+           (printf "(%s)\n" reason)))))))
 
-(defn pinpoint [spec x]
-  (pinpoint-out (s/explain-data spec x) :root x))
+(defn pinpoint
+  ([spec x] (pinpoint spec x {}))
+  ([spec x opts]
+   (pinpoint-out (s/explain-data spec x)
+                 (merge {:root x} opts))))
