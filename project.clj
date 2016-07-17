0d1a5cb5181714593a5af3d75af471562b5f52c7
OHTA Shogo <athos0220@gmail.com>
2016-07-17 16:57:02 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pinpointer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Pinpointer makes it easy to grasp which part of data is violating spec conformance."
+  :url "https://github.com/athos/Pinpointer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
