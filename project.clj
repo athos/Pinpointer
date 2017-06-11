7f4a78302f2f80da0040040bf61357d0738a9bd0
OHTA Shogo <athos0220@gmail.com>
2017-06-11 16:59:27 +0900

    Use Fipp as pretty printer

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [;; clj stuff
                  [org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/spec.alpha "0.1.123"]
+                 [fipp "0.6.8"]
                  [spectrace "0.1.0-SNAPSHOT"]
                  [rewrite-clj "0.5.1"]
                  [clansi "1.0.0"]
